babab8137719a113d88137185415c0ca89001ad7
Caio <caio_g_franco@hotmail.com>
2022-09-30 10:26:48 -0300

    Refactored TM logic, added UI

--- LFTC Simulator/Assets/Tape.cs
+++ LFTC Simulator/Assets/Tape.cs
@@ -1,4 +1,5 @@
 using Assembly_CSharp;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -10,11 +11,12 @@ using UnityEngine;
 public partial class Tape : MonoBehaviour
 {
     // Unity properties
-    public string TapeString = "";
-    public Vector3 OffSet = new Vector3();
+    public string u_TapeString = "test";
+    public Vector3 u_SymbolOffSet = new Vector3(2, 0, 0);
     public Symbol Symbol = null;
-    private List<Symbol> Symbols = new List<Symbol>();
+    private List<Symbol> u_SymbolsList = new List<Symbol>();
     public TMP_Text UIRules;
+    public TMP_Text UINextRule;
     public TMP_Text UICurrentState;
 
 
@@ -22,44 +24,33 @@ public partial class Tape : MonoBehaviour
     private bool m_IsActive { get; set; } = true;
     private string m_InicialState { get; set; }
     private string m_CurrentState { get; set; }
+    private Rule m_CurrentRule { get; set; }
     private List<State> m_States { get; set; } = new List<State>();
     private string m_HeadBuffer { get; set; }
-    private int m_StringIndex { get; set; }
+    private int m_HeadIndex { get; set; }
     private string m_FinalState { get; set; }
 
     // Start is called before the first frame update
     void Start()
     {
-        int i = 0;
-        Vector3 position = transform.position;
-        m_StringIndex = 0;
-        m_HeadBuffer = TapeString[m_StringIndex].ToString();
+        Debug.Log("TM program loading initialized...");
 
-        // Instanciate tape Symbols 
-        foreach (char c in TapeString)
+        Vector3 position = transform.position;
+        foreach (char c in u_TapeString)
         {
-            var newSymbol = Instantiate<Symbol>(Symbol);
-            newSymbol.SetChar(c);
-            newSymbol.transform.Translate(position);
-            newSymbol.transform.parent = this.transform;
-            Symbols.Add(newSymbol);
-            position += OffSet;
-            i++;
+            InstanciateNewSymbol(c, position);
+            position += u_SymbolOffSet;
         }
 
-        // Generate Turing Machine program
         var lines = File.ReadLines(@"A:\Repos\Playground\LFTC-Simulator\LFTC Simulator\Program.txt");
-
-        m_InicialState = m_CurrentState = lines.ElementAt(1).ToString();
-        UICurrentState.SetText(m_InicialState);
-
+        var initialState = lines.ElementAt(1).ToString();
+        var finalState = lines.ElementAt(2).ToString();
         var allStates = lines.ElementAt(0).Split(' ');
-        m_FinalState = lines.ElementAt(2).ToString();
         var allRules = lines.Skip(3).ToList();
 
         foreach (string state in allStates)
         {
-            bool isFinal = state == m_FinalState ? true : false;
+            bool isFinal = state == finalState ? true : false;
 
             List<string> stateRules = new List<string>();
 
@@ -77,9 +68,61 @@ public partial class Tape : MonoBehaviour
             m_States.Add(new State(state, isFinal, stateRules));
         }
 
-        Debug.Log("MT program Loaded");
+        Debug.Log("TM program Loaded, Initialazing...");
 
+        m_InicialState = m_CurrentState = initialState;
+        m_FinalState = finalState;
+        m_HeadIndex = 0;
+
+        Debug.Log("TM Initialized, reading first symbol...");
+
+        ReadSymbol();
+        LoadNextRule();
         UIRules.SetText(BuildUiRulesText());
+        LoadUI();
+
+        Debug.Log("TM ready. Waiting for player...");
+
+        if (!m_IsActive)
+        {
+            StopTM();
+        }
+    }
+
+    private void StopTM()
+    {
+        Debug.Log("The Turing Machine has come to a stop");
+        m_IsActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("User input detected");
+            NextMove();
+        }
+    }
+
+    void NextMove()
+    {
+        if (!m_IsActive)
+        {
+            StopTM();
+            return;
+        }
+        MoveHead();
+        ReadSymbol();
+        LoadNextRule();
+        LoadUI();
+        // Wait for player next move
+    }
+
+    private void LoadUI()
+    {
+        UINextRule.SetText($"{m_CurrentState} {m_CurrentRule.RuleToString()}");
+        UICurrentState.SetText(m_CurrentState);
     }
 
     private string BuildUiRulesText()
@@ -98,17 +141,7 @@ public partial class Tape : MonoBehaviour
         return result.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Debug.Log("User input detected");
-            NextMove();
-        }
-    }
-
-    void NextMove()
+    private void LoadNextRule()
     {
         if (m_CurrentState == m_FinalState)
         {
@@ -123,54 +156,42 @@ public partial class Tape : MonoBehaviour
             return;
         }
 
-        var currentRule = currentState.Rules.Find(rule => rule.ReadChar == m_HeadBuffer);
-        if (currentRule == null)
+        m_CurrentRule = currentState.Rules.Find(rule => rule.ReadChar == m_HeadBuffer);
+        if (m_CurrentRule == null)
         {
             m_IsActive = false;
             return;
         }
-
-        var stringArr = TapeString.ToCharArray();
-        var writeChar = currentRule.WriteChar.ToCharArray()[0];
-
-        stringArr[m_StringIndex] = writeChar;
-
-        if (m_StringIndex >= TapeString.Length)
-        {
-            var newSymbol = Instantiate<Symbol>(Symbol);
-            newSymbol.SetChar(writeChar);
-            newSymbol.transform.Translate(new Vector3(2, 0, 0));
-            newSymbol.transform.parent = this.transform;
-            Symbols.Add(newSymbol);
-        }
-        else
-        {
-            Symbols[m_StringIndex].SetChar(writeChar);
-        }
-
-        TapeString = stringArr.ArrayToString();
-
-        m_CurrentState = currentRule.TargetState;
-        UICurrentState.SetText(m_CurrentState);
-
-        Move(currentRule.MoveRight);
-
-        if (m_StringIndex >= TapeString.Length)
-        {
-            m_HeadBuffer = "!";
-            TapeString = TapeString + "!";
-        }
-        else
-        {
-            m_HeadBuffer = TapeString[m_StringIndex].ToString();
-        }
     }
 
-    void Move(bool right)
+    private void ReadSymbol()
     {
-        Vector3 direction = new Vector3(2, 0, 0);
-        transform.Translate(right ? -direction : direction);
-        if (right) m_StringIndex++; 
-        else m_StringIndex--;
+        if (m_HeadIndex >= u_SymbolsList.Count)
+        {
+            InstanciateNewSymbol('!', Vector3.zero);
+        }
+        if (m_HeadIndex < 0)
+        {
+            StopTM();
+        }
+        m_HeadBuffer = u_SymbolsList[m_HeadIndex].Content.text;
+    }
+
+    void MoveHead()
+    {
+        m_CurrentState = m_CurrentRule.TargetState;
+        u_SymbolsList[m_HeadIndex].SetChar(m_CurrentRule.WriteChar.ToCharArray()[0]);
+        transform.Translate(m_CurrentRule.MoveRight ? -u_SymbolOffSet : u_SymbolOffSet);
+        if (m_CurrentRule.MoveRight) m_HeadIndex++; 
+        else m_HeadIndex--;
+    }
+
+    private void InstanciateNewSymbol(char c, Vector3 position)
+    {
+        var newSymbol = Instantiate<Symbol>(Symbol);
+        newSymbol.SetChar(c);
+        newSymbol.transform.Translate(position);
+        newSymbol.transform.parent = this.transform;
+        u_SymbolsList.Add(newSymbol);
     }
 }
